41cb674fd19352d9d96a9ff0fd9c9ca88eea1073
kaoxi998533 <93632656+kaoxi998533@users.noreply.github.com>
2025-12-18 22:35:19 +0800

    Done searching functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search'
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <main>
-      <div className='pattern'/>
+      <div className='pattern' />
       <div className='wrapper'>
         <header>
+          <img src='./hero.png' alt='Hero Banner' />
           <h1>Find the <span className='text-gradient'>Movies</span> You'll Like Without Hassle</h1>
         </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        <h1 className='text-white'>{searchTerm}</h1>
+
       </div>
     </main>
   )
