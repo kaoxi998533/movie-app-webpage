fceab862e7566b78c110bee0d1ef152e09b659ad
kaoxi998533 <93632656+kaoxi998533@users.noreply.github.com>
2025-12-18 21:45:15 +0800

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div className='pattern'/>
+      <div className='wrapper'>
+        <header>
+          <h1>Find the <span className='text-gradient'>Movies</span> You'll Like Without Hassle</h1>
+        </header>
+      </div>
+    </main>
+  )
+}
+
+export default App
